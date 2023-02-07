f4042b1d5124103185fd3cedebda44ce905d4375
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-08 01:02:22 +0530

    feat(clipboard): implemented copy to clipboard

---
+++ src/components/utils/copy/Copy.tsx
@@ -0,0 +1,29 @@
+import { Copy } from "phosphor-react";
+
+export const CopyButton = ({
+  content,
+  onClick = () => {},
+  onError,
+}: {
+  content: any;
+  onClick?: () => void;
+  onError?: (err: Error) => void;
+}) => {
+  const copy = () => {
+    navigator.clipboard
+      .writeText(JSON.stringify(content))
+      .then(onClick)
+      .catch((e) => {
+        if (onError) {
+          onError(e);
+        } else {
+          console.log(e);
+        }
+      });
+  };
+  return (
+    <button onClick={copy}>
+      <Copy size={20} color="grey" />
+    </button>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chepo/",
+  base: "/chepo/", // modified lines
   plugins: [react()],
 });
