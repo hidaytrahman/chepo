baa132dd38ab102e27b19e46beacbb06b9b7809a
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-07 22:57:17 +0530

    feat(ui): improvements and icon

---
+++ src/components/utils/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Chepo</h1>
+      {/* <div>
+    <img src={hamhu} />
+  </div> */}
+    </header>
+  );
+};
+
+export default Header;
