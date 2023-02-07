8982f962ac420dd17e7075555c00c7581549d886
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-07 21:13:32 +0530

    feat(mock): prettify json data of user

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import JsonViewer from "./components/utils/jsonViewer/JsonViewer";
+import { githubUser, todos, users } from "./model/user/users";
 
 function App() {
   return (
     <div className="App">
       <h1>Chepo</h1>
+
+      <JsonViewer data={users} title="User" />
+      <JsonViewer data={githubUser} title="Github user" />
+      <JsonViewer data={todos} title="Todo" />
     </div>
   );
 }
---
+++ src/model/user/users.ts
@@ -0,0 +1,47 @@
+import { User } from "./types";
+
+export const githubUser = {
+  login: "hidaytrahman",
+  id: 9712111,
+  avatar_url: "https://avatars.githubusercontent.com/u/9712111?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hidaytrahman",
+  html_url: "https://github.com/hidaytrahman",
+  followers_url: "https://api.github.com/users/hidaytrahman/followers",
+  following_url:
+    "https://api.github.com/users/hidaytrahman/following{/other_user}",
+  received_events_url:
+    "https://api.github.com/users/hidaytrahman/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Hidayt Rahman",
+  company: "@qatalog",
+  blog: "https://hidaytrahman.github.io",
+  location: "New Delhi , IND",
+  email: null,
+  hireable: true,
+  bio: "I'm a JavaScript developer who enjoys creating products to improve the productivity and usability of tech in overall capacity.",
+  twitter_username: "hidaytrahman",
+  public_repos: 39,
+  public_gists: 9,
+  followers: 21,
+  following: 19,
+  created_at: "2014-11-13T05:25:43Z",
+  updated_at: "2023-01-20T12:57:03Z",
+};
+export const users: User[] = [
+  {
+    first_name: "Roy",
+    last_name: "Wilson",
+    gender: "male",
+    address: "NH52, India",
+  },
+];
+
+export const todos: any = [
+  {
+    id: "12",
+    title: "Lets have some coffee",
+    isCompleted: false,
+  },
+];
