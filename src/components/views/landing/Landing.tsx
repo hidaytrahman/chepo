fce8b5297f910401be1e5e2a224f7ee0f5deaff4
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-12 02:19:46 +0530

    added search input

--- src/components/views/landing/Landing.tsx
+++ src/components/views/landing/Landing.tsx
@@ -204,7 +204,7 @@ export default function Landing() {
         <label htmlFor="search">Search Data</label>
         <input
           className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          type="text"
+          type="search"
           placeholder="Search"
           list="mockdata"
           id="search"
