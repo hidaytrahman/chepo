d2262416f36a93225d37b87b15b88a2946f255b7
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-12 02:03:56 +0530

    feat(search): mock data

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,13 @@ import "./App.css";
 import JsonViewer from "./components/utils/jsonViewer/JsonViewer";
 import Landing from "./components/views/landing/Landing";
 import { AppContext, appReducer, initialAppState } from "./context";
-import { todos, users } from "./model";
+// import { todos, users } from "./model";
 import { videoPlayer } from "./model/misc";
 
 function App() {
   const [state, dispatch] = useReducer<any>(appReducer, initialAppState);
   return (
-    <AppContext.Provider
-      value={{
-        isSearching: state?.isSearching,
-        setSearchingState: (value: boolean) => {
-          dispatch();
-        },
-      }}
-    >
+    <>
       {/* Need to configure router */}
       {/* <Router>
         <Routes>
@@ -27,17 +20,10 @@ function App() {
         </Routes>
       </Router> */}
 
-      <Landing />
-
       <div className="App">
-        <JsonViewer data={todos} title="Todo" />
-        <JsonViewer data={videoPlayer} title="videoPlayer" />
-        <JsonViewer data={users} title="User" />
-        {/* <JsonViewer data={githubUser} title="Github user" /> */}
-        {/* <JsonViewer data={photosShort} title="Photos Short" /> */}
-        {/* <JsonViewer data={photosFull} title="Photos Full" /> */}
+        <Landing />
       </div>
-    </AppContext.Provider>
+    </>
   );
 }
 
