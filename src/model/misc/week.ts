e0904164f56ed6165d20664a79ba5baa94e87001
Hidayt Rahman <hidaytrahman@gmail.com>
2023-03-10 23:11:43 +0530

    feat(mock): weekds short and full added

---
+++ src/model/misc/week.ts
@@ -0,0 +1,11 @@
+export const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const weekShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
