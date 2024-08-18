e9619ffe1bd77772d713d72c408a20fccf822121
Hidayt Rahman <hidaytrahman@gmail.com>
2024-08-18 14:38:08 +0530

    feat: mock for products

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,29 +1,22 @@
-import {
-  videoPlayer,
-  audioPlayer,
-  slideSettings,
-  languages,
-  months,
-  monthsShort,
-  week,
-  weekShort,
-} from "./misc";
-import { photosFull, photosShort, todos, users } from "./user/users";
+import { videoPlayer, audioPlayer, slideSettings, languages, months, monthsShort, week, weekShort } from './misc';
+import { photosFull, photosShort, todos, users } from './user/users';
 
-import { githubUser } from "./user/users";
+import { githubUser } from './user/users';
+import { products } from './misc/products';
 
 export const modal: any = {
-  photosFull,
-  photosShort,
-  todos,
-  users,
-  githubUser,
-  videoPlayer,
-  audioPlayer,
-  slideSettings,
-  languages,
-  months,
-  monthsShort,
-  week,
-  weekShort,
+	photosFull,
+	photosShort,
+	todos,
+	users,
+	githubUser,
+	videoPlayer,
+	audioPlayer,
+	products,
+	slideSettings,
+	languages,
+	months,
+	monthsShort,
+	week,
+	weekShort,
 };
