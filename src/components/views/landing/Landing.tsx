f21c227a33b7c7e3a35a4773cb3813d3a8c6ba5b
Hidayt Rahman <hidaytrahman@gmail.com>
2024-08-19 15:54:23 +0530

    chore: product hunt embed

--- src/components/views/landing/Landing.tsx
+++ src/components/views/landing/Landing.tsx
@@ -51,6 +51,21 @@ export default function Landing() {
 					<div className='flex lg:flex-1'>
 						<img className='logo' loading='lazy' src={logo} alt='chepo logo' />
 					</div>
+
+					<a
+						href='https://www.producthunt.com/posts/chepo?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chepo'
+						target='_blank'
+						referrerPolicy='no-referrer'
+					>
+						<img
+							src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=480988&theme=light'
+							alt='Chepo - Get&#0032;generated&#0032;data&#0032;with&#0032;a&#0032;single&#0032;click | Product Hunt'
+							style={{ width: '250px', height: '54px' }}
+							width='250'
+							height='54'
+						/>
+					</a>
+
 					{/* <div className='flex lg:hidden'>
 						<button
 							type='button'
